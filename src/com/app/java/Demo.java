8ca1a30184ae8f6788bf6b0be74a024a68bd0ef0
anudeep5e7 <anudeeprachamalla@gmail.com>
2022-09-08 23:32:48 +0530

    projcreated

---
+++ src/com/app/java/Demo.java
@@ -0,0 +1,16 @@
+package com.app.java;
+
+public class Demo {
+
+	public void show(String str)
+	{
+		System.out.println("welcome to java world");
+	}
+	
+	public static void main(String[] args) {
+		
+		Demo d=new Demo();
+		
+		d.show("aa");
+	}
+}
